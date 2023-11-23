eb73b5e5522a59ec46e7d802cf50e376aff536a8
ayandas-96rev <sa.kumardas5@gmail.com>
2023-11-24 00:50:14 +0530

    xyz

---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div className='w-full h-full bg-bg-page-dark border-2 border-solid border-black flex justify-between overflow-hidden'>
+        <section className='w-3/5 h-full bg-slate-500 border-2 mt-2 p-6 overflow-y-auto'>
+            s
+        </section>
+
+        <section className='w-2/5 h-full bg-slate-500 border-2 mt-2'>
+            s
+        </section>
+
+        
+        
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'brand-dark-blue': '#071A53',
+        'bg-page-dark': '#f1f6ff',
+      }
+    },
+  },
+  plugins: [],
+}
+
