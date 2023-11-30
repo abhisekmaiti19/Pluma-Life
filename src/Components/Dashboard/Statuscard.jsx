f9bcac20fe693678664b8166b699355171b11595
Abhisek <abhisekmaiti19@gmail.com>
2023-11-30 19:06:38 +0530

    all done dashboard

--- src/Components/Dashboard/Statuscard.jsx
+++ src/Components/Dashboard/Statuscard.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import face from "../../assets/statusface.png";
 
-export default function Statuscard({content}) {
+export default function Statuscard({ content }) {
   return (
-    <div className={`min-w-full min-[431px]:min-w-[260px] min-h-md bg-white border-[1px] shadow-sm rounded-xl flex flex-grow flex-col ${content?'':'animate-pulse'}`}>
-      <div className={`w-full h-20 bg-${content?'brand-dark-blue':'gray-300'} rounded-xl flex items-center px-2 justify-center gap-5`}>
-        <img src={content?content.image||face:''} alt="" className="w-8 h-8"/>
+    <div
+      className={`min-w-full min-[431px]:min-w-[260px] min-h-md bg-white border-[1px] shadow-sm rounded-xl flex flex-grow flex-col ${
+        content ? "" : "animate-pulse"
+      } border border-brand-dark-blue border-1`}
+    >
+      <div
+        className={`w-full h-20 bg-${
+          content ? "brand-dark-blue" : "gray-300"
+        } rounded-xl flex items-center px-2 justify-center gap-5`}
+      >
+        <img
+          src={content ? content.image || face : ""}
+          alt=""
+          className="w-8 h-8"
+        />
         <h1 className="text-md text-white font-bold">{content?.title}</h1>
       </div>
-      <div className="flex-auto w-full flex items-center justify-center py-2">
+      <div className="flex-auto w-full flex items-center justify-center py-2 ">
         <table className="text-xs font-medium mx-2">
-        {Object.keys(content?.data || {}).map(key=> (
+          {Object.keys(content?.data || {}).map((key) => (
             <tr className="w-full">
-              <td className="p-2 min-w-[140px] text-blue-900 font-bold">{key}</td>
+              <td className="p-2 min-w-[140px] text-blue-900 font-bold">
+                {key}
+              </td>
               <td className="p-2 whitespace-nowrap">{content.data[key]}</td>
             </tr>
           ))}
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import mic from "../assets/anouncemic.png";
 import imp from "../assets/important.png";
 import Anouncement from "../Components/Anouncement";
@@ -10,43 +10,42 @@ import hi from "../assets/hi.png";
 import Statuscard from "../Components/Dashboard/Statuscard";
 import Groupcard from "../Components/Dashboard/Groupcard";
 
-import bag from '../assets/statusbag.png'
-import money from '../assets/statusmoney.png'
+import bag from "../assets/statusbag.png";
+import money from "../assets/statusmoney.png";
 
 export default function Dashboard() {
   const buisnessStatus = {
-    title: 'Buisness Status',
+    title: "Buisness Status",
     image: bag,
     data: {
-      'My Rank': '102',
-      'Buisness Ratio': '1069:600',
-      'Performance Bonus': '23456 bv',
-    }
-  }
+      "My Rank": "102",
+      "Buisness Ratio": "1069:600",
+      "Performance Bonus": "23456 bv",
+    },
+  };
   const currentStatus = {
-    title: 'Current Status',
+    title: "Current Status",
     data: {
-      'Total Direct': '10',
-      'Downline Team': '23',
-    }
-  }
+      "Total Direct": "10",
+      "Downline Team": "23",
+    },
+  };
   const payoutStatus = {
-    title: 'Payout Status',
+    title: "Payout Status",
     image: money,
-    data : {
-      'Total Payout': '10367 pv',
-      'Total Performance Payout': '926793 pv'
-    }
-  }
-
+    data: {
+      "Total Payout": "10367 pv",
+      "Total Performance Payout": "926793 pv",
+    },
+  };
 
   const ref = useRef(null);
-  const scrollToRight = ()=>{
+  const scrollToRight = () => {
     ref.current.scrollLeft += 260;
-  }
-  const scrollToLeft = ()=>{
+  };
+  const scrollToLeft = () => {
     ref.current.scrollLeft -= 260;
-  }
+  };
 
   return (
     <div className="w-full bg-bg-page-dark flex justify-between relative overflow-hidden">
@@ -54,25 +53,62 @@ export default function Dashboard() {
         <div className="w-full relative flex flex-col gap-2">
           <Message message={"Abhisek"} img={hi} />
           <div className="w-full rounded-xl overflow-hidden relative">
-            <div className="absolute left-0 h-full w-0 hover:w-40 top-0 bg-gradient-to-r from-brand-dark-blue to-transparent rounded-l-xl opacity-0 hover:opacity-100 transition-all hidden sm:block">
-              <button className="text-white h-full p-2 w-40" onClick={scrollToLeft}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-              </svg>
-              </button>
+            <div className="w-full h-full absolute opacity-0 hover:opacity-100 transition-all hidden sm:block duration-700">
+              <div
+                className={`absolute left-0 h-full w-40 top-0  rounded-l-xl  transition-all duration-700 ease-in-out`}
+              >
+                <button
+                  className="text-white h-full p-2 w-40 bg-gradient-to-r from-brand-dark-blue/30  hover:from-brand-dark-blue duration-800 delay-500 ease-in-out"
+                  onClick={scrollToLeft}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="w-8 h-8 hover:rotate-180 hover:scale-[1000%] duration-700"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 19.5L8.25 12l7.5-7.5"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div
+                className={`absolute right-0 h-full w-40 top-0  rotate-180 transition-all duration-700 ease-in-out`}
+              >
+                <button
+                  className="text-white h-full p-2 w-40 bg-gradient-to-r from-brand-dark-blue/30 to-transparent rounded-l-xl duration-700 hover:from-brand-dark-blue"
+                  onClick={scrollToRight}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="w-8 h-8 hover:rotate-180 hover:scale-[1000%] duration-700 "
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 19.5L8.25 12l7.5-7.5"
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
-            <div className="absolute right-0 h-full top-0 bg-gradient-to-l from-brand-dark-blue to-transparent rounded-l-xl opacity-0 hover:opacity-100 transition-all hidden sm:block">
-              <button className="text-white h-full p-2 w-20 hover:w-40 flex justify-end items-center transition-all duration-200 ease-in-out" onClick={scrollToRight}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-              </svg>
-              </button>
-            </div>
-            <div className="w-full overflow-x-auto customscroll scroll-smooth" ref={ref}>
+            <div
+              className="w-full overflow-x-auto customscroll scroll-smooth"
+              ref={ref}
+            >
               <div className="gap-6 flex justify-between">
-                <Statuscard content={currentStatus}/>
-                <Statuscard content={payoutStatus}/>
-                <Statuscard content={buisnessStatus}/>
+                <Statuscard content={currentStatus} />
+                <Statuscard content={payoutStatus} />
+                <Statuscard content={buisnessStatus} />
               </div>
             </div>
           </div>
@@ -89,6 +125,7 @@ export default function Dashboard() {
       <section className="lg:w-1/4 lg:bottom-0 overflow-y-auto lg:block min-[200px]:hidden fixed top-[70px] right-[1%]">
         <div className="customscroll flex flex-col gap-4 p-6 m-4 border-2 bg-white rounded-xl">
           <Anouncement compimg={mic} text="" />
+
           <Importantcard
             compimg={imp}
             text=""
